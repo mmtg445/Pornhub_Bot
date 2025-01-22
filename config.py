cd689af4010f85792e1a7c01d5d6123fa0db16aa
mmtg445 <mmtg445@gmail.com>
2025-01-22 21:56:23 +0600

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
 
-    API_ID = os.getenv("API_ID")
-    API_HASH = os.getenv("API_HASH")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    API_ID = os.getenv("API_ID", "26649585")
+    API_HASH = os.getenv("API_HASH", "588a3ea6fd01ae88bd2e10fed7d55b2c")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8102315483:AAEju5xATi7ePCxtJk9rVwmmLiPt-ITd6kY")
