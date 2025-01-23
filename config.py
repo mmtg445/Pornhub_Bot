f8da39019bd847c46edaf1e4aadcf08b73df0366
mmtg445 <mmtg445@gmail.com>
2025-01-23 12:43:22 +0600

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class Config:
     API_ID = os.getenv("API_ID", "26649585")
     API_HASH = os.getenv("API_HASH", "588a3ea6fd01ae88bd2e10fed7d55b2c")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "8102315483:AAEju5xATi7ePCxtJk9rVwmmLiPt-ITd6kY")
+    LOG_CHANNEL_ID = -1002420046638
